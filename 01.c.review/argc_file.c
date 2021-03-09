9a3bcd2049f2b6dbc43645f05b948c2dbbfd139e
xhl <xhl@me.com>
2021-03-09 13:19:35 +0800

    complied argc_file

--- 01.c.review/argc_file.c
+++ 01.c.review/argc_file.c
@@ -1,24 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main(int argc,char *argc[])
+int main(int argc,char *argv[])
 {
 	FILE *in,*out;
 	char ch;
 	if(argc != 3)
 	{
-		printf("tags:x file1 file2\n");
+		printf("tips:x file1 file2\n");
 		exit(0);
 	}
-	if((in = fopen(argc[1],"rb")) == NULL)
+	if((in = fopen(argv[1],"rb")) == NULL)
 	{
-		printf("%s open failed!\n",argc[1]);
+		printf("%s open failed!\n",argv[1]);
 		exit(0);
 	}
-	if((out = fopen(argc[2],"wb")) == NULL)
+	if((out = fopen(argv[2],"wb")) == NULL)
 	{
-		printf("%s open failed!\n",argc[2]);
+		printf("%s open failed!\n",argv[2]);
 		exit(0);
 	}
+	while(!feof(in))
+	{
+		ch = fgetc(in);
+		fputc(ch,out);
+	}
+	fclose(in);
+	fclose(out);
+}
 
 
 
